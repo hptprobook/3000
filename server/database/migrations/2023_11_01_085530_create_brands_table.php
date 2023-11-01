37168407eb10d9a66715fc0b4315ddd9bdd5eb59
beovan <85023866+beovan@users.noreply.github.com>
2023-11-01 17:22:43 +0700

    Update 2023_11_01_085530_create_brands_table.php

--- server/database/migrations/2023_11_01_085530_create_brands_table.php
+++ server/database/migrations/2023_11_01_085530_create_brands_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name', 128);
-            $table->unsignedTinyInteger('parent_id')->nullable();
-            $table->string('icon_url', 255);
+            $table->unsignedTinyInteger('parent_id');
+            $table->string('icon_url', 255)->nullable();
         });
     }
 
