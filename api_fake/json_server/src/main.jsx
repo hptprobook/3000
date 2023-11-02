afd87faf153cc26b140a38d58d94fa860ead192f
beovan <85023866+beovan@users.noreply.github.com>
2023-11-02 09:51:35 +0700

    Delete api_fake directory

--- api_fake/json_server/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import JsonServer from '../JsonServer.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <JsonServer/>
-)
