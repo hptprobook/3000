538782656552f6715836ccbf535cd0a98e7bd5f2
Phan Thanh Hoa <hptprobook@gmail.com>
2023-11-04 10:01:49 +0700

    Update Home.page.jsx

--- admin/src/pages/Home/Home.page.jsx
+++ admin/src/pages/Home/Home.page.jsx
@@ -1,12 +1,12 @@
 import { Typography } from "@mui/material";
 import React, { useEffect } from "react";
-import authService from "../../services/auth.service";
+import AuthService from "../../services/auth.service";
 
 export default function HomePage() {
     useEffect(() => {
         const fetchApi = async () => {
             try {
-                const result = await authService();
+                const result = await AuthService.login();
             } catch (error) {
                 console.error("Error:", error);
             }
