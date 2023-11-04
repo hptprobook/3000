d44f7a674cf287452060c0412621e774942994eb
Nguyen Van Tin <bguyt46@gmail.com>
2023-11-04 14:24:04 +0700

    update

--- admin/src/pages/Home/Home.page.jsx
+++ admin/src/pages/Home/Home.page.jsx
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import authService from "../../services/auth.service";
-
+import { fetchAllUsers } from "../../redux/slices/userSlice";
 export default function HomePage() {
     // useEffect(() => {
     //     const fetchApi = async () => {
