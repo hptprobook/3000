917064e018d0c22959e4298836b887ec2a73121d
Phan Thanh Hoa <hptprobook@gmail.com>
2023-11-06 22:24:55 +0700

    tester

--- admin/src/routes/dashboard.route.jsx
+++ admin/src/routes/dashboard.route.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/Home/Home.page";
 import Error404Page from "../pages/Error/Error404Page";
 import UserPage from "../pages/User/List/ListUserPage";
+import AddUserPage from "../pages/User/Add/AddUserPage";
 
 function DashboardRoute() {
     return (
@@ -10,6 +11,7 @@ function DashboardRoute() {
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Error404Page />} />
             <Route path="user/list" element={<UserPage />} />
+            <Route path="user/create" element={<AddUserPage />} />
         </Routes>
     );
 }
