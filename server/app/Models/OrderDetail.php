7bacef9f06490ea92711ef99dcd474df7a757369
Phan Thanh Hoa <hptprobook@gmail.com>
2023-11-06 22:05:38 +0700

    api

--- server/app/Models/OrderDetail.php
+++ server/app/Models/OrderDetail.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'discount'];
+
+    public $timestamps = false;
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -51,4 +51,14 @@ class User extends Authenticatable
     {
         return $this->hasMany(Address::class);
     }
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
