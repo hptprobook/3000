756dd8ca0aa57d0fd82e12bc66136d76242a7eb1
Nguyen Van Tin <bguyt46@gmail.com>
2023-11-22 18:27:24 +0700

    update

--- admin/src/pages/Product/Create/CreateProductPage.jsx
+++ admin/src/pages/Product/Create/CreateProductPage.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Box, colors } from "@mui/material";
+import HeaderPage from "../../../components/common/HeaderPage/HeaderPage";
 import "./style.css";
 
 export default function CreateProductPage() {
-    return <div>CreateProductPage</div>;
+    return (
+        <Box>
+            <HeaderPage
+                namePage={"Tạo mới"}
+                Breadcrumb={["Sản phẩm", "Tạo"]}
+            />
+        </Box>
+    );
 }
